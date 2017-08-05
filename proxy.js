f8e3aed6d6901c5845f6d0de7847a7bd79896d12
Ivan Pantic <ivanpantic82@gmail.com>
2017-08-05 15:19:10 +0200

    Works

--- proxy.js
+++ proxy.js
@@ -1,24 +1,59 @@
+const libUrl = require('url');
+
 const proxy = require('express-http-proxy');
+const morgan = require('morgan');
 
 const app = require('express')();
 
 const UNEDDIT_HOST = 'uneddit.com';
 const HOSTNAME = 'uneddit.pantas.net';
 const SCRIPTS = [
-	'phase1',
-	'phase2'
+	'phase_one',
+	'phase_two'
 ];
+const PORT = process.env.PORT || 15080;
 
-const scriptHandler = proxy(UNEDDIT_HOST, {
-	userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
-		const script = proxyResData.toString('utf8');
-		const replaced = script.replace(/uneddit.com/gm, HOSTNAME);
-		return JSON.stringify(data);
-	}
+const PROXY_OPTS = {
+https: false,
+	proxyReqPathResolver: function(req) {
+		return libUrl.parse(req.url).path;
+	},
+};
+
+app.use(morgan('tiny'));
+
+app.get('/', (req, res, next) => {
+	return res.send(`
+		Proxy for uneddit.com
+		Until they fix their HTTPS stuff.
+	`);
 });
 
 SCRIPTS.forEach(script => {
-	app.use('/' + script, scriptHandler);
+	const opts = Object.assign({
+			userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
+				
+				// Replace url-s
+				let script = proxyResData.toString('utf8');
+				script = script.replace(new RegExp(UNEDDIT_HOST, 'gm'), HOSTNAME);
+
+				// Fix trailing / in URL concatenation
+				script = script.replace(/(var newurl = [^;]+;)/gm, match => {
+					return match + ' newurl = newurl.replace(/\\/^/gm, "");';
+				});
+
+				console.log(`Modified`, userReq.url);
+				return script;
+			},
+		},
+		PROXY_OPTS
+	);
+	app.get('/' + script, proxy(UNEDDIT_HOST, opts));
+	console.log(`Special proxy handler for /${script}`);
 });
 
-app.use('/', proxy(UNEDDIT_HOST));
+app.use('/', proxy(UNEDDIT_HOST, PROXY_OPTS));
+
+app.listen(PORT, () => {
+	console.log('Listening on port ' + PORT);
+});
